9fb2aea9c5c779c2728073c59eb4feb98451053e
yeseniav32 <95379817+yeseniav32@users.noreply.github.com>
2022-01-13 08:47:21 -0600

    Add files via upload

---
+++ module.py
@@ -0,0 +1,9 @@
+from turtle import width
+
+
+length = input("Enter the Length of the house in feet: ")
+width = input("Enter the Width of the house in feet7.5:")
+
+area = (length*width)**2
+
+result = "The squared area of the house is: {}".format(area)
