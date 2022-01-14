6f4d8b7e0c61c834d1613787d08419925c561455
yeseniav32 <95379817+yeseniav32@users.noreply.github.com>
2022-01-13 19:48:03 -0600

    Delete module.py

--- module.py
+++
@@ -1,9 +0,0 @@
-from turtle import width
-
-
-length = input("Enter the Length of the house in feet: ")
-width = input("Enter the Width of the house in feet7.5:")
-
-area = (length*width)**2
-
-result = "The squared area of the house is: {}".format(area)
